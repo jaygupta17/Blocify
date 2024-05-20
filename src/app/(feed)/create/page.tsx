ec2ef4673e8f28b8ef2441017221174ddd6b3e90
OmChillure <omchillure4@gmail.com>
2024-05-20 16:29:19 +0530

    profile, ui

---
+++ src/app/(feed)/create/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className='h-[100vh] w-full flex justify-center items-center z-100'>
+      Create
+    </div>
+  )
+}
--- src/utils/gemini.ts
+++ src/utils/gemini.ts
@@ -1,13 +1,24 @@
-import { GoogleGenerativeAI } from "@google/generative-ai"
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import { HfInference } from "@huggingface/inference";
 
 const genAI = new GoogleGenerativeAI(process.env.NEXT_PUBLIC_GEMINI_API_KEY!);
+const hf = new HfInference(process.env.NEXT_PUBLIC_HF_API_KEY!);
 
-export const textTotext =async (inp: string) =>{
-  const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+export const textTotext = async (inp: string) => {
+  const model = genAI.getGenerativeModel({ model: "gemini-pro" });
   const result = await model.generateContent(inp);
-  const response =result.response;
+  const response = result.response;
   const text: string = response.text();
 
-//   setresponse(text);
-  return text
-}
+  //   setresponse(text);
+  return text;
+};
+
+export const textToImage = async (inp: string) => {
+  let model = await hf.textToImage({
+    inputs: inp,
+    model: "stabilityai/stable-diffusion-2",
+    parameters: { negative_prompt: "blurry" },
+  });
+  return URL.createObjectURL(model);
+};
