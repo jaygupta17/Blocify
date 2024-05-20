45a1ff2bdd5fd30dee01742a628d0694bdd16bb3
Jay Gupta <jayajaygupta16@gmail.com>
2024-05-20 08:58:50 +0000

    ws

--- src/app/(feed)/home/page.tsx
+++ src/app/(feed)/home/page.tsx
@@ -1,16 +1,19 @@
+import { CarouselSpacing } from '@/components/ui/StoryCarousel'
 import React, { use } from 'react'
-import { getUsers } from "../../../lib/data";
-import { User } from '@/lib/model';
+// import { getUsers } from "../../../lib/data";
+// import { User } from '@/lib/model';
 async function Home() {
-  let users : any = await getUsers();
-  console.log();
+  // let users : any = await getUsers();
+  // console.log();
   return (
     <div className='h-[100vh] w-full flex justify-center items-center z-100'>
-      {users.map((user : any)=>{
+      {/* {users.map((user : any)=>{
         return(<div key={user.id || user.username} className='h-20 w-50 bg-red-100'>
           {user.username}
         </div>);
-      })}
+      })} */}
+      <CarouselSpacing/>
+    
     </div>
   )
 }
